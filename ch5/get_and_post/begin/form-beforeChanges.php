c1d12217324e66a422ee4da1e8375a4609af1cc1
Rick Maclean <rick_maclean@sil.org>
2015-03-24 14:17:15 -0500

    Two first sections of CH5 added

---
+++ ch5/get_and_post/begin/form-beforeChanges.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contact Us</title>
+<link href="../styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>Contact Us</h1>
+<form name="contact" method="post" action="">
+    <p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+    </p>
+    <p>
+        <label for="comments">Comments:</label>
+        <textarea name="comments" id="comments"></textarea>
+    </p>
+    <p>
+        <input type="submit" name="send" id="send" value="Send Comments">
+    </p>
+</form>
+<pre>
+
+</pre>
+</body>
+</html>
---
+++ ch5/get_and_post/end/form.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contact Us</title>
+<link href="../styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>Contact Us</h1>
+<form name="contact" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+    </p>
+    <p>
+        <label for="comments">Comments:</label>
+        <textarea name="comments" id="comments"></textarea>
+    </p>
+    <p>
+        <input type="submit" name="send" id="send" value="Send Comments">
+    </p>
+</form>
+<pre>
+<?php
+if ($_GET) {
+	echo 'Contents of the $_GET array: <br>';
+	print_r($_GET);
+} elseif ($_POST) {
+	echo 'Contents of the $_POST array: <br>';
+	print_r($_POST);
+}
+?>
+</pre>
+</body>
+</html>
